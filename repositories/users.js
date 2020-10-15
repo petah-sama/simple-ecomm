61cff184975c605a362521e0e79b7829ad5af76e
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-15 16:49:27 +0100

    added new methods, update, delete, getOne, and assigned a random id to each user

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
     constructor(filename) {
@@ -24,22 +25,56 @@ class UsersRepository {
     }
 
     async create(attrs) {
+        attrs.id = this.randomId();
+
         const records = await this.getAll();
         records.push(attrs);
         await this.writeAll(records);
     }
     
     async writeAll(records) {
-        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+        await fs.promises.writeFile(
+            this.filename, 
+            JSON.stringify(records, null, 2)
+        );
+    }
+
+    randomId() {
+        return crypto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+
+        return records.find(record => record.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+
+        const filteredRecords = records.filter(record => record.id !== id);
+
+        await this.writeAll(filteredRecords);
+    }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`)
+        }
+
+        Object.assign(record, attrs);
+
+        await this.writeAll(records);
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
-    await repo.create({ email: 't@t.com', password: 'password' });
-    const users = await repo.getAll();
 
-    console.log(users);
+    await repo.update('e0ae4701', { password: 'mypass' });
 }
 
 test();
