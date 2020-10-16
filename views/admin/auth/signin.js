2f722008560632ecd7c68983445d4b532a7445f4
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-16 16:49:57 +0100

    structure refactored and added express validator and some basic signup validators

---
+++ views/admin/auth/signin.js
@@ -0,0 +1,15 @@
+const layout = require ('../layout');
+
+module.exports = () => {
+    return layout({ 
+        content: `
+            <div>
+                <form method="POST">
+                    <input name="email" placeholder="Email" />
+                    <input name="password" placeholder="Password" />
+                    <button>Sign In</button>
+                </form>
+            </div>
+        `
+    });
+}
