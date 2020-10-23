9e297aba9450630f6b76a4254306412fc5e04936
Pedro Batista <pedrobatista.420@gmail.com>
2020-10-23 18:15:57 +0100

    delete a product now working and products.json file updated with better seeds

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -73,5 +73,11 @@ router.post(
     }
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+    await productsRepo.delete(req.params.id);
+
+    res.redirect('/admin/products');
+});
+
 
 module.exports = router;
